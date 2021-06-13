0c167335f201f90ac575ed935adbd8f15f02b8dc
Baklaikin Kostiantyn <baklaikin@gmail.com>
2021-06-13 20:19:00 +0300

    gallery without keyboard action

--- gallery.js
+++ gallery.js
@@ -8,13 +8,11 @@ const refs = {
   img: document.querySelector('.lightbox__image'),
   lightboxContent: document.querySelector('.lightbox__content'),
   closeLightboxBtn: document.querySelector('.lightbox__button'),
-  
 };
-
 const markup = markupConstructor(galleryItems);
 
 function markupConstructor(galleryItems) {
-   return galleryItems.map(({ preview, original, description }) => {
+  return galleryItems.map(({ preview, original, description}, idx) => {
         return `
         <li class="gallery__item">
     <a
@@ -25,6 +23,7 @@ function markupConstructor(galleryItems) {
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
+      data-index-number="${idx}"
       alt="${description}"
     />
   </a>
@@ -34,52 +33,47 @@ function markupConstructor(galleryItems) {
 };
 
 refs.gallery.insertAdjacentHTML('afterbegin', markup);
-// const link = document.querySelectorAll('.gallery__link');
-// const item = document.querySelector('.gallery__item');
 
 let modalPicture = refs.img;
-// let modalPictureLink = refs.link;
+
+const imageData = document.querySelectorAll('.gallery__image');
+
 const onGalleryClick = (e) => {
   e.preventDefault();
   const imageSource = e.target.dataset.source;
-  console.log(e.target);
+  const imageIndex = e.target.dataset.indexNumber;
   const imageAlt = e.target.alt;
   refs.lightBox.classList.toggle('is-open');
 
   modalPicture.src = imageSource;
   modalPicture.alt = imageAlt;
 
-  return modalPicture;
+  const onKeyPress = (e) => {
+    if (e.key !== "Escape" && e.key !== "ArrowRight" && e.key !== "ArrowLeft") {
+      return;
+    };
+    if (e.key === "Escape") {
+      modalClose();
+    }
+ 
+    if (e.key === "ArrowRight") {
+    }
+  };
+  window.addEventListener('keydown', onKeyPress);
 };
 
 refs.gallery.addEventListener('click', onGalleryClick);
 
 const modalClose = (e) => {
   refs.lightBox.classList.remove('is-open');
+  refs.overlay.addEventListener('click', modalClose, { once: true });
+  
 
   modalPicture.src = '';
   modalPicture.alt = '';
 };
 
-
-
-const onKeyPress = (e) => {
-  if (e.key !== "Escape" && e.key !== "ArrowRight" && e.key !== "ArrowLeft") {
-    return;
-  }
-  const isActive = document.querySelector('.is-open');
-  console.log(isActive.children);
-  if (e.key === "Escape") {
-    refs.lightBox.classList.remove('is-open');
-    modalPicture.src = '';
-    modalPicture.alt = '';
-  }
-
-  if (e.key === "ArrowRight") {
-    
-  }
-}
-window.addEventListener('keydown', onKeyPress);
-
-refs.overlay.addEventListener('click', modalClose);
 refs.closeLightboxBtn.addEventListener('click', modalClose);
+
+
+
